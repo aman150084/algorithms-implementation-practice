4b9fbec42a55c036952874bae1fe895928ff2504
aman150084 <aman150084@gmail.com>
2017-01-13 02:15:37 +0530

    added the runtime error code of CAPIMOVE

--- codechef/capimove.c
+++ codechef/capimove.c
@@ -1,12 +1,54 @@
 #include<stdio.h>
 #include<stdlib.h>
-int findmax(int* pop, int* non, int n, int count){
+int max2(int a, int b){
+    if(a>=b) return a;
+    else return b;
+}
+int findmax(long long int *array, int*non, int n, int count){
+    
+    int i = 0;
+    long long int* array2;
+    array2 = (long long int*)malloc(n*sizeof(long long int));
+    for(;i<n;i++){
+        int j=0;
+        array2[i] = array[i];
+        for(;j<count;j++){
+            if(non[j]==i){
+                array2[i]=0;
+            }
+        }
+    }
+    int max= array2[0];
+    // printf("%dmax \n", max);
+    i=0;
+    while(i<(n-1)){
+        if(array2[max]<array2[i+1]){
+            max = i+1;
+        }
+        i++;
+    }
+    return max;
+}                              
+
+/*int findmax(long long int* pop, int* non, int n, int count){
     int i = 0;
     int j = 0;
+    int p=0;
+    int max;
     for(i=0; i<n; i++){
         for(;j<count;j++){
             if(i==non[j]||i+1==non[j]){
-                continue;
+                p=1;
+                break;
+            }
+        }
+        if(p==1)
+            continue;
+        
+        if(array[i]>array[max]){
+
+    }
+}*/
 int main(){
     int t;
     scanf("%d", &t);
@@ -27,11 +69,13 @@ int main(){
             scanf("%d %d",&a, &b);
             a += -1;
             b += -1;
-            adj[*n + b] = 1;
+            adj[a*n + b] = 1;
             adj[b*n + a] = 1;
         }
+        nonallowed = (int*)malloc(n*sizeof(int));
+
         for(i=0;i<n;i++){
-            nonallowed = (int*)malloc(n*sizeof(int));
+        //    nonallowed = (int*)malloc(n*sizeof(int));
             for(j=0;j<n;j++){
                 nonallowed[j] = -1;
             }
@@ -39,18 +83,16 @@ int main(){
             int count = 1;
             for(j=0;j<n;j++){
                 if(adj[n*i + j] == 1){
-                    nonallowed[count]=0;
+                    nonallowed[count]=j;
                     count++;
                 }
             }
-            printf("%d ",findmax());
+          //  for(j=0;j<count;j++){
+            //    printf("a%d\n", nonallowed[j]);}
+            printf("%d ",findmax(pop, nonallowed, n, count)+1);
+
         }
         printf("\n");
         }
-    return 0;
+        return 0;
 }
-
-
-
-            
-
